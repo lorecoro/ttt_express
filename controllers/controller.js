108e3767651cc45a50fb94b0c2df8c494a3c903f
lorenzo <lorenzo@coronati.com>
2020-07-12 18:30:09 +0300

    multiplayer game

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,29 +1,76 @@
 const gameModel = require('../models/game');
 const async = require('async');
 
+exports.player = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    async.series({
+        found: (callback) => {
+            gameModel.find({}, callback);
+        }
+    }, (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            if (results) {
+                if (results.found[0]) {
+                    if (!results.found[0].playerx) {
+                        res.json({player: 'x'});
+                        gameModel.findOneAndUpdate(
+                            {}, 
+                            {'$set': {'playerx': true}}, 
+                            {upsert: true}, 
+                            (err) => {
+                                if (err) console.log(err);
+                            }
+                        );
+                    }
+                    else if (!results.found[0].playero) {
+                        res.json({player: 'o'});
+                        gameModel.findOneAndUpdate(
+                            {}, 
+                            {'$set': {'playero': true}}, 
+                            {upsert: true}, 
+                            (err) => {
+                                if (err) console.log(err);
+                            }
+                        );
+                    }
+                    else {
+                        // Both players have started.
+                        res.json({player: ''});
+                    }
+                }
+            }
+        }
+    });
+};
+
 exports.store = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     async.series({
-        delete: (callback) => {
-            gameModel.deleteMany({}, callback);
-        },
-        create: () => {
-            const gameInstance = new gameModel({
+        upsert: () => {
+            const game = {
                 player: req.body.player,
                 matrix: req.body.matrix,
                 status: req.body.status,
-            });
-            gameInstance.save((err) => {
-                if (err) {
-                    console.log(err);
-                    return handleError(err);
+            };
+            gameModel.findOneAndUpdate(
+                {}, 
+                {'$set': game}, 
+                {upsert: true}, 
+                (err, results) => {
+                    if (err) {
+                        res.send(err);
+                    } else {
+                        res.send(results);
+                    }
                 }
-            });
+            );
         } 
     }, (err) => {
         if (err) throw err;
     });
-    res.send('ok');
 };
  
 exports.retrieve = (req, res) => {
@@ -33,6 +80,57 @@ exports.retrieve = (req, res) => {
             gameModel.find({}, callback);
         }
     }, (err, results) => {
-        res.json(results.found[0]);
+        if (err) {
+            console.log(err);
+        }
+        else {
+            if (results) {
+                if (results.found[0]) {
+                    let player = 'x';
+                    if (results.found[0].status === 'ongoing') {
+                        player = results.found[0].player === 'x' ? 'o' : 'x';
+                    }
+                    const cookie = ['player=' + player];
+                    res.setHeader('Set-Cookie', cookie);
+                    res.json(results.found[0]);
+                }
+                else {
+                    // No documents in mongo collection: create one. Use upsert in order to prevent double documents.
+                    const game = {
+                        player: 'x',
+                        playerx: false,
+                        playery: false,
+                        matrix: [],
+                        status: 'new',
+                    };
+                    gameModel.findOneAndUpdate(
+                        {}, 
+                        {'$set': game}, 
+                        {upsert: true}, 
+                        (err, results) => {
+                            if (err) {
+                                res.send(err);
+                            } else {
+                                res.send(results);
+                            }
+                        }
+                    );
+                }
+            }
+        }
     });
 };
+
+exports.reset = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    gameModel.deleteMany(
+        {}, 
+        (err, results) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(results);
+            }
+        }
+    );
+}
--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const controller = require('../controllers/controller');
 
 router.get('/board', controller.retrieve);
+router.get('/player', controller.player);
+router.get('/reset', controller.reset);
 router.post('/board', controller.store);
 
 module.exports = router;
